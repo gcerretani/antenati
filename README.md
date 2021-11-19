9edb428c9d314fd1582a276ba242f448644f2a18
Giovanni Cerretani <giovanni.cerretani@gmail.com>
2021-11-19 14:31:21 +0100

    tentative to work on new architecture

--- README.md
+++ README.md
@@ -3,23 +3,20 @@ A tool to download data from the *[Portale Antenati](http://www.antenati.san.ben
 
 Since the website tends to be pretty slow in the evening, we present a script to help the retrieval of the documents for your family tree. The script allows you to download **all the images of multiple archive at the same time** (launching multiple instance of the script), whithout any human action. Just launch the script, and have a coffee while it downloads all the stuff for you.
 
-## Known issue
-**Currently, it does not work after November 2021 major restyle of *Portale Antenati***. More details on [this issue](https://github.com/gcerretani/antenati/issues/5).
-
 ## Usage 
 Just run the script putting as first argument the first page of a collection you want to download:
 
-    ./antenati.py <URL of the first page>
+    ./antenati.py <URL of the album>
 
-The files are put to a new folder named as *COMUNE_TYPE_YEAR* of the downloaded archive.
+The files are put to a new folder named as *ARCHIVE_PLACE_YEAR_TYPE* of the downloaded archive.
 
 ### Example:
-In the website, navigate to the archive you want to download. For example, for the people born in Montalcino in 1832, you should find the page:
+In the website, navigate to the archive you want to download. For example, for the people born in Viareggio in 1807 you should find the page:
 
-[http://dl.antenati.san.beniculturali.it/v/Archivio+di+Stato+di+Firenze/Stato+civile+della+restaurazione+1816-1860/Montalcino+provincia+di+Siena/Nati/1832/179/](http://dl.antenati.san.beniculturali.it/v/Archivio+di+Stato+di+Firenze/Stato+civile+della+restaurazione+1816-1860/Montalcino+provincia+di+Siena/Nati/1832/179/)
+[https://www.antenati.san.beniculturali.it/detail-registry/?s_id=19944549](https://www.antenati.san.beniculturali.it/detail-registry/?s_id=19944549)
 
 Then, copy the link to the first page, and call the script with that link as argument:
 
-    ./antenati.py http://dl.antenati.san.beniculturali.it/v/Archivio+di+Stato+di+Firenze/Stato+civile+della+restaurazione+1816-1860/Montalcino+provincia+di+Siena/Nati/1832/179/005178080_00303.jpg.html
+    ./antenati.py https://www.antenati.san.beniculturali.it/detail-registry/?s_id=19944549
 
-The results will be placed in a folder named *Montalcino_provincia_di_Siena_Nati_1832*.
+The results will be placed in a folder named *archivio-di-stato-di-lucca-stato-civile-napoleonico-viareggio-1807-nati*.
--- antenati.py
+++ antenati.py
@@ -8,12 +8,13 @@ __copyright__   = "Copyright (c) 2018, MIT License"
 
 import urllib3
 import html.parser
+import json
 import sys
 import os
 import re
 import click
 import threading
-
+import slugify
 
 class Downloader(threading.Thread):
     def __init__ (self, pool, url, filename):
@@ -30,52 +31,44 @@ class Downloader(threading.Thread):
         print('Done ', self._filename)
 
 
-class ImageHTMLParser(html.parser.HTMLParser):
+class ImageHTMLParser():
     def __init__(self, pool):
         super().__init__()
         self._pool = pool
-        self._filename = None
         self._threads = []
     def get_threads(self):
         return self._threads
-    def set_filename(self, name):
-        self._filename = 'img_archive_' + name + '.jpg'
-    def handle_starttag(self, tag, attrs):
-        if tag == 'a':
-            attr_dict = dict(attrs)
-            url = attr_dict['href']
-            t = Downloader(self._pool, url, self._filename)
+    def get_file(self, url, name):
+            filename = 'img_archive_' + name + '.jpg'
+            t = Downloader(self._pool, url, filename)
             self._threads.append(t)
 
 
-class UrlHTMLParser(html.parser.HTMLParser):
-    def __init__(self):
-        super().__init__()
-        self._next = None
-    def set_next(self, next):
-        self._next = next
-    def get_next(self):
-        return self._next
-    def handle_starttag(self, tag, attrs):
-        if tag == 'a':
-            attr_dict = dict(attrs)
-            if attr_dict['class'] == 'next':
-                url = attr_dict['href']
-                self._next = url
-
-
 def main():
-    connection_pool = urllib3.HTTPConnectionPool('dl.antenati.san.beniculturali.it', maxsize = 10)
+
+    connection_pool = urllib3.HTTPSConnectionPool('iiif-antenati.san.beniculturali.it', maxsize = 10)
     img_parser = ImageHTMLParser(connection_pool)
-    url_parser = UrlHTMLParser()
     
-    url_parser.set_next(sys.argv[1])
-    
-    splitting = re.split('[_/?.]', url_parser.get_next())
-    
-    html_element = splitting.index('html')
-    gallery_name_elements = splitting[10 : html_element - 3]
-    foldername = '_'.join(gallery_name_elements).replace('+', '_')
+    pool_manager = urllib3.PoolManager()
+    r = pool_manager.request('GET', sys.argv[1])
+
+    manifest = None
+
+    for line in r.data.decode('utf-8').split('\n'):
+        if 'manifestId' in line:
+            print(line)
+            splitting = re.split('[\']', line)
+            manifest = splitting[1]
+
+    if not manifest:
+        print('No manifest found')
+        return
+
+    r = pool_manager.request('GET', manifest)
+
+    manifest_json = json.loads(r.data.decode('utf-8'))
+
+    foldername = slugify.slugify(manifest_json['label'] + '-' + manifest_json['metadata'][1]['value'])
     
     if os.path.exists(foldername):
         if not click.confirm('Directory ' + foldername + ' already exists. Do you want to copy images to this directory?'):
@@ -86,24 +79,11 @@ def main():
         
     os.chdir(foldername)
 
-    stop = False
+    for img_desc in manifest_json['sequences'][0]['canvases']:
+        url = img_desc['images'][0]['resource']['@id']
+        name = slugify.slugify(img_desc['label'])
+        img_parser.get_file(url, name)
     
-    while not stop:
-        stop = True
-        r = connection_pool.request('GET', url_parser.get_next())
-        splitting = re.split('[_/?.]', url_parser.get_next())
-        html_element = splitting.index('html')
-        file_name_elements = splitting[html_element - 3 : html_element - 1]
-        local_filename = '_'.join(file_name_elements)
-        img_parser.set_filename(local_filename)
-    
-        for line in r.data.decode('utf-8').split('\n'):
-            if 'zoomAntenati1' in line:
-                img_parser.feed(line)
-            if 'successivo' in line:
-                stop = False
-                url_parser.feed(line)
-                
     for t in img_parser.get_threads():
         t.join()
 
