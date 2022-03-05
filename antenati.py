993ea6a94475f472dd81875d6b9b6f7724711043
Giovanni Cerretani <giovanni.cerretani@gmail.com>
2022-03-05 09:07:26 +0100

    using real user agent to avoid new server block

--- antenati.py
+++ antenati.py
@@ -37,7 +37,8 @@ class AntenatiDownloader:
     @staticmethod
     def __http_headers():
         return make_headers(
-            accept_encoding=True
+            accept_encoding=True,
+            user_agent='Mozilla/5.0 (Android 4.4; Mobile; rv:41.0) Gecko/41.0 Firefox/41.0'
         )
 
     @staticmethod
