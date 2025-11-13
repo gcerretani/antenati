0c26df2c34f0d6bd88fce2b1870a1073ac40cb6c
Giovanni Cerretani <giovanni.cerretani@gmail.com>
2025-11-13 21:58:30 +0100

    testing selenium

--- selenium_utils.py
+++ selenium_utils.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+import undetected as uc
 
 def get_page_with_selenium(url: str, headers: dict) -> str:
     """
@@ -13,12 +14,12 @@ def get_page_with_selenium(url: str, headers: dict) -> str:
     :return: HTML content of the page
     """
     chrome_options = Options()
-    #chrome_options.add_argument('--headless')  # Run in headless mode
+    chrome_options.add_argument('--headless')  # Run in headless mode
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument('--window-size=1920,1080')  # Set window size
-    #chrome_options.add_argument(f'--user-agent={headers["User-Agent"]}')  # Avoid headless detection
-    driver = webdriver.Chrome(options=chrome_options)
+    chrome_options.add_argument(f'--user-agent={headers["User-Agent"]}')  # Avoid headless detection
+    driver = uc.Chrome(options=chrome_options)
     try:
         if headers and 'Referer' in headers:
             driver.execute_cdp_cmd('Network.setExtraHTTPHeaders', {'headers': {'Referer': headers['Referer']}})
