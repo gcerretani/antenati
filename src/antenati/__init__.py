26f1da7e5918819b729ea8907a1b138e52b06dd3
Giovanni Cerretani <cerrigno@gmail.com>
2026-09-20 09:11:32 +0200

    Sort public exports for Ruff

--- src/antenati/__init__.py
+++ src/antenati/__init__.py
@@ -32,11 +32,11 @@ from antenati.errors import DownloadFailedError, ThreadError
 __all__ = [
     'DEFAULT_N_THREADS',
     'DEFAULT_SIZE',
-    'Downloader',
     'DownloadFailedError',
     'DownloadItem',
     'DownloadPlan',
     'DownloadReport',
+    'Downloader',
     'PageFailure',
     'ProgressBar',
     'ThreadError',
